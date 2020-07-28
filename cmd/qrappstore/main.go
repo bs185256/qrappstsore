1ed08c3e5ca799e60a93795d6db4e2e7de4ba9ef
Dasika, SaiKiran <SaiKiran.Dasika@ncr.com>
2020-07-28 11:57:07 -0400

    qrappstore: added app and snapshot packages

--- cmd/qrappstore/main.go
+++ cmd/qrappstore/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
+	"time"
 
+	"github.com/dg185200/qrappstore/pkg/app"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -17,21 +22,44 @@ var (
 
 func init() {
 	flag.StringVar(&addr, "addr", "localhost:9000", "host+port for the server to bind to")
-	flag.BoolVar(&debug, "debug", true, "enable this flag for all debug logging")
+	flag.BoolVar(&debug, "debug", false, "enable this flag for all debug logging")
 }
 
 func main() {
 	flag.Parse()
 	r := mux.NewRouter()
-	r.Use(requestLoggingMiddleWare)
-	r.HandleFunc("/_ah/healthz", healthHandler).Methods("GET")
+	r.Use(timer, requestLoggingMiddleWare)
+	r.HandleFunc("/_ah/healthz", healthHandler).Methods(http.MethodGet)
+
+	apiRouter := r.PathPrefix("/api").Subrouter()
+	apiRouter.Use(contentTypeJSON)
+	apiRouter.HandleFunc("/apps", func(w http.ResponseWriter, r *http.Request) {
+		if err := json.NewEncoder(w).Encode(app.Default); err != nil {
+			log.Println(err)
+		}
+	}).Methods(http.MethodGet)
 
 	log.Println("starting server on:", addr)
-	log.Fatal(http.ListenAndServe(addr, r))
+	log.Fatal(http.ListenAndServe(addr, handlers.LoggingHandler(os.Stdout, r)))
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
+}
+
+func contentTypeJSON(nh http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		nh.ServeHTTP(w, r)
+	})
+}
+
+func timer(nh http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		nh.ServeHTTP(w, r)
+		log.Println("took", time.Since(start), "to fullfill request", r.URL.EscapedPath())
+	})
 }
 
 func requestLoggingMiddleWare(nh http.Handler) http.Handler {
