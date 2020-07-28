5af4faeee639d5b16128a6dcff95889ee373b252
Dasika, SaiKiran <SaiKiran.Dasika@ncr.com>
2020-07-28 09:55:58 -0400

    qrappstore: skeleton http server

--- cmd/qrappstore/main.go
+++ cmd/qrappstore/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	// host+port
+	addr  string
+	debug bool
+)
+
+func init() {
+	flag.StringVar(&addr, "addr", "localhost:9000", "host+port for the server to bind to")
+	flag.BoolVar(&debug, "debug", true, "enable this flag for all debug logging")
+}
 
 func main() {
-	fmt.Println("welcome to qrappstore")
+	flag.Parse()
+	r := mux.NewRouter()
+	r.Use(requestLoggingMiddleWare)
+	r.HandleFunc("/_ah/healthz", healthHandler).Methods("GET")
+
+	log.Println("starting server on:", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok"))
+}
+
+func requestLoggingMiddleWare(nh http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if debug {
+			b, _ := httputil.DumpRequest(r, true)
+			log.Printf("\n----------\n%s\n----------\n", b)
+		}
+		// move on the the next handler
+		nh.ServeHTTP(w, r)
+	})
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dg185200/qrappstore
+
+go 1.14
+
+require github.com/gorilla/mux v1.7.4
